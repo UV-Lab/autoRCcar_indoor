d2985ecfad69c07ac0b17c2d7a9c8b489848813e
gspark <pks87@nate.com>
2023-08-05 22:33:37 +0900

    modified script

--- init_build.sh
+++ init_build.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+# CPU 모델 확인
+CPU_MODEL=$(cat /proc/cpuinfo | grep "model name" | head -1 | awk -F'i[3579]-' '{split($2, a, " "); sub(/[^0-9]+$/, "", a[1]); print a[1]}')
+CPU_VER=${CPU_MODEL//[!0-9]/}
+CPU_NOW=$(cat /proc/cpuinfo | awk -F: '/model name/ {print $2; exit}')
+
+## Livox-SDK2
+echo "Would you want to build include Livox-SDK2?"
+echo "You need to build Livox-SDK2 once for the first time."
+echo "If your answer is no, only the ROS2 packages is build."
+read -p "Answer yes or no : " answer
+
+
+if [[ "$answer" == "YES" || "$answer" == "Yes" || "$answer" == "yes" ]]; then
+
+    echo "Important) You need to enter your sudo password during build."
+    sleep 3
+    
+    cd Livox-SDK2
+    if [ -d "build" ]; then
+        rm -rf build
+    fi
+    mkdir build && cd build
+    if [[ "$CPU_VER" -lt "5000" ]]; then
+        cmake .. && make -j 2
+    else
+        cmake .. && make -j
+    fi
+    sudo make install
+    cd ../..
+
+elif [[ "$answer" == "NO" || "$answer" == "No" || "$answer" == "no" ]]; then
+    echo ""
+
+else
+    echo "entered incorrectly"
+    exit 1
+fi
+
+
+## ROS2 Packages
 if [ -x "$(command -v ./ros2/src/livox_ros_driver2/build.sh)" ]; then
     ./ros2/src/livox_ros_driver2/build.sh humble
 else
@@ -15,10 +55,7 @@ if [ $? -ne 0 ]; then
 fi
 source install/setup.bash
 
-# CPU 모델 확인
-CPU_MODEL=$(cat /proc/cpuinfo | grep "model name" | head -1 | awk -F'i[3579]-' '{split($2, a, " "); sub(/[^0-9]+$/, "", a[1]); print a[1]}')
-CPU_VER=${CPU_MODEL//[!0-9]/}
-CPU_NOW=$(cat /proc/cpuinfo | awk -F: '/model name/ {print $2; exit}')
+
 # CPU 모델 비교
 if [[ "$CPU_VER" -lt "5000" ]]; then
     ## intel 5세대보다 작으면
