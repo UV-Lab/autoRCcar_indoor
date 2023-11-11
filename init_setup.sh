1801b47e5234fe992512d2ae2b8ace591c58e558
GSPark <pks87@nate.com>
2023-11-11 22:43:53 +0900

    divide bash script

---
+++ init_setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+### Dependencies
+
+## LIO-SAM (ros2)
+sudo apt install -y ros-humble-perception-pcl \
+  	   ros-humble-pcl-msgs \
+  	   ros-humble-vision-opencv \
+  	   ros-humble-xacro
+
+## LIO-SAM (gtsam)
+sudo add-apt-repository ppa:borglab/gtsam-release-4.1
+sudo apt install -y libgtsam-dev libgtsam-unstable-dev
+
+## oCam
+sudo apt-get install libv4l-dev libudev-dev
+
+## GCS
+pip install numpy PyQt5 pyqtgraph
+
+## Livox-SDK2
+cd Livox-SDK2
+if [ -d "build" ]; then
+    rm -rf build
+fi
+mkdir build && cd build
+if [[ "$CPU_VER" -lt "5000" ]]; then
+    cmake .. && make -j 2
+else
+    cmake .. && make -j
+fi
+sudo make install
+cd ../..
+
+./ros2_build.sh
