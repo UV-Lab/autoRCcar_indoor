0596a43e9a30719d7fc320a9d78fef2de9863d02
luke7637 <luke7637@gmail.com>
2024-12-07 14:33:09 +0900

    Apply suggestions from code review
    Co-authored-by: hyunjin <41056497+dlfksj@users.noreply.github.com>

--- ros2/src/autorccar_gcs/launch/autorccar_gcs.launch.py
+++ ros2/src/autorccar_gcs/launch/autorccar_gcs.launch.py
@@ -7,7 +7,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    planning_control_node = Node(
+    gcs_node = Node(
         package="autorccar_gcs",
         executable="autorccar_gcs",
         name="autorccar_gcs",
@@ -15,6 +15,6 @@ def generate_launch_description():
         output="screen",
     )
     ld = LaunchDescription()
-    ld.add_action(planning_control_node)
+    ld.add_action(gcs_node)
 
     return ld
